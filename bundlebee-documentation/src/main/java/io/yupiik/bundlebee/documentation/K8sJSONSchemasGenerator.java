3415d3077f5acdae73718e63a4072b51c7be1794
Romain Manni-Bucau <rmannibucau@gmail.com>
2022-01-23 20:47:43 +0100

    fixing headers

--- bundlebee-documentation/src/main/java/io/yupiik/bundlebee/documentation/K8sJSONSchemasGenerator.java
+++ bundlebee-documentation/src/main/java/io/yupiik/bundlebee/documentation/K8sJSONSchemasGenerator.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2021 - Yupiik SAS - https://www.yupiik.com
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package io.yupiik.bundlebee.documentation;
 
 import lombok.extern.java.Log;
@@ -139,8 +154,16 @@ public class K8sJSONSchemasGenerator implements Runnable {
                         .uri(URI.create(url))
                         .build(),
                 ofString());
-        if (response.statusCode() != 200) {
-            throw new IllegalStateException("Invalid response: " + response);
+
+        switch (response.statusCode()) {
+            case 200:
+                // all good
+                break;
+            case 404:
+                log.info(() -> "No spec for '" + url + "'");
+                return;
+            default:
+                throw new IllegalStateException("Invalid response: " + response);
         }
 
         final JsonObject spec;
