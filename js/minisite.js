5b64bf317c1c6dd4887398d637dd5504c032548f
Romain Manni-Bucau <rmannibucau@gmail.com>
2022-12-27 17:01:06 +0100

    Updating the website with version 0.0.2-SNAPSHOT // 2022-12-27T16:58:30.417719376+01:00

--- js/minisite.js
+++ js/minisite.js
@@ -254,7 +254,7 @@ $(document).ready(function () {
 
   function loadSearchIndex() {
       return new Promise(function(ok, ko) {
-          $.getJSON('//www.yupiik.io/bundlebee/search.json', function(index) {
+          $.getJSON('http://localhost:4200/search.json', function(index) {
               ok(window.minisiteFuseFactory ? window.minisiteFuseFactory(index) : new Fuse(index, {
                   shouldSort: true,
                   includeMatches: true,
