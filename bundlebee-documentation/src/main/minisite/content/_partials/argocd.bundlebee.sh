7348197b6fb9ab7e89e49354d2575c13272e0553
Romain Manni-Bucau <romain.manni@orangelogic.com>
2025-11-28 11:56:21 +0100

    site update

--- bundlebee-documentation/src/main/minisite/content/_partials/argocd.bundlebee.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-java -Dio.yupiik.bundlebee.level=WARNING -jar \
-    /opt/yupiik/bundlebee/lib/bundlebee.jar process \
-    --injectTimestamp false --injectBundleBeeMetadata false \
-    --output stdout \
-    --bundlebee-kube-namespace "${ARGOCD_APP_NAMESPACE}" \
-    --from "$ARGOCD_APP_SOURCE_PATH" --manifest "${ARGOCD_APP_SOURCE_PATH}/bundlebee/manifest.json" --alveolus "$ARGOCD_APP_NAME"
